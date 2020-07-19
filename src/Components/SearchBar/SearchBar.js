e7dcd7c6f4cd64a557c0bfdaddbe63ca69be9503
melaniesarah <melaniesarah@gmail.com>
2020-07-18 20:13:27 -0500

    Convert SearchBar.js from html React component

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,4 +1,17 @@
-<div class="SearchBar">
-    <input placeholder="Enter A Song, Album, or Artist" />
-    <button class="SearchButton">SEARCH</button>
-</div>
+import React, { Component } from 'react';
+
+
+class SearchBar extends Component {
+    render() {
+        return (
+            <div className="SearchBar">
+                <input placeholder="Enter A Song, Album, or Artist" />
+                <button className="SearchButton">SEARCH</button>
+            </div>
+        );
+    }
+};
+
+export default SearchBar;
+
+
