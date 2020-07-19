62d2bc9b2a1846120ff09a2f9fad06307917087b
melaniesarah <melaniesarah@gmail.com>
2020-07-18 20:20:14 -0500

    convert Playlist.js from html to React component

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,5 +1,17 @@
-<div class="Playlist">
-    <input value="New Playlist" />
-    <!-- Add a TrackList component -->
-  <button class="Playlist-save">SAVE TO SPOTIFY</button>
-</div>
+import React, { Component } from 'react';
+import TrackList from '../TrackList/TrackList';
+import './Playlist.css';
+
+class Playlist extends Component {
+  render() {
+    return (
+      <div className="Playlist">
+        <input value="New Playlist" />
+        <TrackList />
+        <button className="Playlist-save">SAVE TO SPOTIFY</button>
+      </div>
+    );
+  }
+}
+
+export default Playlist;
