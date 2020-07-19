59a7b1b1dc6a2010f2d25c61038eeb11fdb4da7c
melaniesarah <melaniesarah@gmail.com>
2020-07-18 20:37:28 -0500

    Render each Track given tracks prop

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Track from '../Track/Track';
 import './TrackList.css';
 
 class TrackList extends Component {
     render() {
         return (
             <div className="TrackList">
+                {this.props.tracks.map(track => {
+                    return <Track key={track.id} track={track} />;
+                })}
             </div>
         );
     }
