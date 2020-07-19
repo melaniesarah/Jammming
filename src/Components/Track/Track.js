fa460bc51a738ac12d2b656cc5472ee7492d2fdb
melaniesarah <melaniesarah@gmail.com>
2020-07-18 20:25:47 -0500

    Convert Track.js from html to React component

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,7 +1,18 @@
-<div class="Track">
-    <div class="Track-information">
-        <h3><!-- track name will go here --></h3>
-        <p><!-- track artist will go here--> | <!-- track album will go here --></p>
-    </div>
-    <button class="Track-action"><!-- + or - will go here --></button>
-</div>
+import React, { Component } from 'react';
+import './Track.css';
+
+class Track extends Component {
+    render() {
+        return (
+            <div className="Track">
+                <div className="Track-information">
+                    <h3>Track name</h3>
+                    <p>Track Artist | Track Album</p>
+                </div>
+                <button className="Track-action">+ or - will go here</button>
+            </div>
+        );
+    }
+}
+
+export default Track;
