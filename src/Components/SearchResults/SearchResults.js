2c30a71784d7cca5e41fc74199490168c0179025
melaniesarah <melaniesarah@gmail.com>
2020-07-18 20:17:14 -0500

    Convert SearchResults from html to React component

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,4 +1,16 @@
-<div className="SearchResults">
-    <h2>Results</h2>
-    <!-- Add a TrackList component -->
-</div>
+import React, { Component } from 'react';
+import TrackList from '../TrackList/TrackList';
+import './SearchResults.css';
+
+class SearchResults extends Component {
+    render() {
+        return (
+            <div className = "SearchResults" >
+                <h2>Results</h2>
+                <TrackList />
+            </div >
+        );
+    }
+}
+
+export default SearchResults;
