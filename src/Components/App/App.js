931a6bccbc2ba92cc485693b7b85ff18e697b955
melaniesarah <melaniesarah@gmail.com>
2020-07-18 20:10:30 -0500

    Update App.js imports, components, and  JSX syntax

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+
 
 function App() {
   return (
     <div>
-      <h1>Ja<span class="highlight">mmm</span>ing</h1>
-      <div class="App">
-        <!-- Add a SearchBar component -->
-        <div class="App-playlist">
-              <!-- Add a SearchResults component -->
-          <!-- Add a Playlist component -->
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        <SearchBar />
+        <div className="App-playlist">
+          <SearchResults />
+          <Playlist />
         </div>
       </div>
     </div>
