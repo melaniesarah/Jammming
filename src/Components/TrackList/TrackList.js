6dc5027dbada7f2af83702988bb0be3ba3ea2d68
melaniesarah <melaniesarah@gmail.com>
2020-07-18 20:22:52 -0500

    Convert TrackList from html to React component

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,3 +1,13 @@
-<div class="TrackList">
-    <!-- You will add a map method that renders a set of Track components  -->
-</div>
+import React, { Component } from 'react';
+import './TrackList.css';
+
+class TrackList extends Component {
+    render() {
+        return (
+            <div className="TrackList">
+            </div>
+        );
+    }
+}
+
+export default TrackList;
