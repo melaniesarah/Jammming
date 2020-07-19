4f5fd1a8a87096e110494da93f1d6439f060a292
melaniesarah <melaniesarah@gmail.com>
2020-07-18 21:20:20 -0500

    Add props to TrackList call

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -7,7 +7,7 @@ class Playlist extends Component {
     return (
       <div className="Playlist">
         <input value="New Playlist" />
-        <TrackList />
+        <TrackList tracks={[]} />
         <button className="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
     );
