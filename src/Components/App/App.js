d66e11723bd2a9f64ca8b841688635112ebd0dd7
melaniesarah <melaniesarah@gmail.com>
2020-07-18 20:31:35 -0500

    Hard code in state searchResults

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,23 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 
-function App() {
-  return (
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div className="App">
-        <SearchBar />
-        <div className="App-playlist">
-          <SearchResults />
-          <Playlist />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        {
+          name: song1,
+          artist: artist1,
+          album: album1
+        },
+        {
+          name: song2,
+          artist: artist2,
+          album: album2
+        },
+        {
+          name: song3,
+          artist: artist3,
+          album: album3
+        },
+        {
+          name: song4,
+          artist: artist4,
+          album: album4
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} />
+            <Playlist />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
